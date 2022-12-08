ee99083679d2c12caaf563b7c546caf884c5c1c5
Mc Gabriel Fernandez <55516426+McGabriel99@users.noreply.github.com>
2022-12-07 18:43:34 -0800

    configuration for header and url for all apps

---
+++ oldScraper/config.py
@@ -0,0 +1,4 @@
+#
+
+HEADERS = {'cookie': 'sessionid=cd46137aee87759ca68f1347'} #cookie to bypass age verification, which is required to view the sysreqs
+APP_URL = "https://store.steampowered.com/app/"
